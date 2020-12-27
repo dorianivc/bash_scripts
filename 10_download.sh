ee35199625897e3d08ff4e2d8e506c2aa976edca
Dorian Vallecillo <dorianivc1@gmail.com>
2020-12-26 23:18:11 -0600

    Condicionales

---
+++ 10_download.sh
@@ -0,0 +1,6 @@
+# !/bin/bash
+# Programa para ejemplificar el uso de la descarga de información desde internet utilizando el comando wget
+# Autor: Dorian Vallecillo Calderon - dorianvic1@gmail.com
+
+echo "Descargar información de internet"
+wget https://downloads.apache.org/tomcat/tomcat-8/v8.5.61/bin/apache-tomcat-8.5.61.zip
